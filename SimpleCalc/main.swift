e5579750e0fa6f5ba8a5b43a832cefc34b96935c
Weschler, Sabrina <weschler@uw.edu>
2015-10-07 12:58:10 -0700

    Created the files for SimpleCalc.

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Weschler, Sabrina on 10/7/15.
+//  Copyright (c) 2015 Weschler, Sabrina. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
