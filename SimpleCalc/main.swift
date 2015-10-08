0bf2dd36e8bf279c7c942d2d7783a6b4872cfef2
Weschler, Sabrina <weschler@uw.edu>
2015-10-07 18:51:07 -0700

    added all the fuctions for the SimpleCalc homework.

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -8,5 +8,97 @@
 
 import Foundation
 
-println("Hello, World!")
+println("Enter an expression separated by returns:")
+
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+func convert(incoming:String) -> Double {
+    return (incoming as NSString).doubleValue
+}
+
+func avg(nums: [Double]) -> Double {
+    var sum = 0.0
+    for num in nums {
+        sum += num
+    }
+    return Double(sum) / Double(nums.count)
+}
+
+func fact(n: Double) -> Double {
+    if n == 0 {
+        return 1
+    } else {
+        return n * fact(n-1)
+    }
+}
+
+
+func basicOperation(firstNumber: Double, operation: String, secondNumber: Double) -> Double {
+    switch operation {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        case "%":
+            return firstNumber % secondNumber
+        default:
+            return 0
+    }
+}
+
+func logic() {
+    let enteredText = split(input()) {$0 == " " }
+    
+    var listOfNumbers: [Double] = []
+    for value in enteredText {
+        listOfNumbers.append(convert(value))
+    }
+    let operation = input()
+    if enteredText.count > 1 {
+        if operation == "avg" {
+            print(avg(listOfNumbers))
+        }else if operation == "count" {
+            print(enteredText.count)
+        } else {
+            print("Number entered could not be processed.")
+        }
+    } else {
+        let firstNumber = convert(enteredText[0])
+        
+        if operation == "fact" {
+            if firstNumber < 0 {
+                print("-\(fact(abs(firstNumber)))")
+            } else {
+                print("results: \(abs(fact(firstNumber)))")
+            }
+        }
+        
+        let secondNumber: Double = convert(input())
+        
+        print("result: \(basicOperation(firstNumber, operation, secondNumber))")
+
+    }
+}
+logic()
+
+
+
+
+
+
+
+
+
+
+
+
 
